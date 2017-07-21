974dade48a4b8a8f405c1ac92556d64862a2ae81
JT <tejadong@gmail.com>
2017-07-21 05:53:26 +0200

    Añadidos parámetros correctos a la config de conexión.

--- src/Jazzyweb/AulasMentor/AlimentosBundle/Config/Config.php
+++ src/Jazzyweb/AulasMentor/AlimentosBundle/Config/Config.php
@@ -5,8 +5,8 @@ namespace Jazzyweb\AulasMentor\AlimentosBundle\Config;
 class Config
 {
     static public $mvc_bd_hostname = "localhost";
-    static public $mvc_bd_nombre   = "alimentos";
+    static public $mvc_bd_nombre   = "appalimentos";
     static public $mvc_bd_usuario  = "root";
-    static public $mvc_bd_clave    = "root";
+    static public $mvc_bd_clave    = "oretania";
     static public $mvc_vis_css     = "estilo.css";
 }
